86ef1a93f1416ed7453bcebf495cdb375296007f
Syed Muhammad Kumail Ali <152736418+Kumaillx@users.noreply.github.com>
2026-07-10 22:03:35 +0500

    project complete

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,112 @@
 #include <Arduino.h>
-#include "BluetoothSerial.h"
-
-BluetoothSerial SerialBT;
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
 
 const int RELAY_PIN = 13;
 bool isDeviceOn = false;
 
-void turnDeviceOff() {
-    digitalWrite(RELAY_PIN, LOW);
-    isDeviceOn = false;
-    Serial.println("[BT] Command Executed: OFF");
-    SerialBT.println("-> Bubbles are now [OFF]");
-}
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
-void turnDeviceOn() {
-    digitalWrite(RELAY_PIN, HIGH);
-    isDeviceOn = true;
-    Serial.println("[BT] Command Executed: ON");
-    SerialBT.println("-> Bubbles are now [ON]");
-}
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+        deviceConnected = true;
+        Serial.println("[BLE] Client connected");
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+        deviceConnected = false;
+        Serial.println("[BLE] Client disconnected");
+    }
+};
+
+class MyCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+        String value = pCharacteristic->getValue();
+
+        if (value.length() > 0) {
+            char incomingChar = toupper(value[0]);
+            Serial.print("[BLE] Command received: ");
+            Serial.println(incomingChar);
+
+            if (incomingChar == 'O') {
+                digitalWrite(RELAY_PIN, HIGH);
+                isDeviceOn = true;
+                Serial.println("[BLE] Device State -> ON");
+                pCharacteristic->setValue("-> Bubbles are now [ON]");
+                pCharacteristic->notify();
+            } else if (incomingChar == 'F') {
+                digitalWrite(RELAY_PIN, LOW);
+                isDeviceOn = false;
+                Serial.println("[BLE] Device State -> OFF");
+                pCharacteristic->setValue("-> Bubbles are now [OFF]");
+                pCharacteristic->notify();
+            }
+        }
+    }
+};
 
 void setup() {
     Serial.begin(115200);
     pinMode(RELAY_PIN, OUTPUT);
     digitalWrite(RELAY_PIN, LOW);
-    SerialBT.begin("ESP32_Bubble_Machine");
-    Serial.println("Bluetooth started. Pair with 'ESP32_Bubble_Machine'");
+
+    // Create the BLE Device
+    BLEDevice::init("ESP32_Bubble_Machine");
+
+    // Create the BLE Server
+    pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new MyServerCallbacks());
+
+    // Create the BLE Service
+    BLEService *pService = pServer->createService(SERVICE_UUID);
+
+    // Create a BLE Characteristic
+    pCharacteristic = pService->createCharacteristic(
+                        CHARACTERISTIC_UUID,
+                        BLECharacteristic::PROPERTY_READ   |
+                        BLECharacteristic::PROPERTY_WRITE  |
+                        BLECharacteristic::PROPERTY_NOTIFY |
+                        BLECharacteristic::PROPERTY_INDICATE
+                      );
+
+    // Add a Descriptor for Notification
+    pCharacteristic->addDescriptor(new BLE2902());
+
+    pCharacteristic->setCallbacks(new MyCallbacks());
+
+    // Start the service
+    pService->start();
+
+    // Start advertising
+    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+    pAdvertising->addServiceUUID(SERVICE_UUID);
+    pAdvertising->setScanResponse(true);
+    pAdvertising->setMinPreferred(0x06);  // help with iPhone connection issues
+    pAdvertising->setMinPreferred(0x12);
+    BLEDevice::startAdvertising();
+    Serial.println("BLE Device started. Search for 'ESP32_Bubble_Machine' via Web Bluetooth!");
 }
 
 void loop() {
-    if (SerialBT.available() > 0) {
-        char incomingChar = SerialBT.read();
-        incomingChar = toupper(incomingChar);
-
-        if (incomingChar == 'O') {          // ON command
-            if (!isDeviceOn) {
-                turnDeviceOn();
-            }
-        } else if (incomingChar == 'F') {    // OFF command
-            if (isDeviceOn) {
-                turnDeviceOff();
-            }
-        }
-        delay(200);
+    // disconnecting
+    if (!deviceConnected && oldDeviceConnected) {
+        delay(500); // give the bluetooth stack a chance to get ready
+        pServer->startAdvertising(); // restart advertising
+        Serial.println("[BLE] Advertising restarted");
+        oldDeviceConnected = deviceConnected;
     }
+    // connecting
+    if (deviceConnected && !oldDeviceConnected) {
+        // do stuff on connection
+        oldDeviceConnected = deviceConnected;
+    }
+    delay(10);
 }
